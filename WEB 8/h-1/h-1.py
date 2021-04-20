dc49f2e970780e479e01bef16b3793e94b0e5742
Alexander Korchuganov <60512317+akorchuganoff@users.noreply.github.com>
2021-04-20 20:52:40 +0700

    Update h-1.py

--- WEB 8/h-1/h-1.py
+++ WEB 8/h-1/h-1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/distribution', methods=['GET', 'POST'])
 def login():
     L = ['sljfvk', 'skdjfnm', 'sdf;jn', 'fdgljsgf', 'slfjn']
     return render_template('distribution.html', list=L)
